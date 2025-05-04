eef68273a2eb9a85735599e023083849c7544add
akhdanfarrosrenoldi <renoldiakhdanfarros@gmail.com>
2025-05-04 08:24:25 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  <>
+    <p>hello raect js</p>
+    <p>hello raect js</p>
+  </>;
+}
+
+export default App;
