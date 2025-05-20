d9d27a8679e3bda983ca28f503358b58c99c0054
akhdanfarrosrenoldi <renoldiakhdanfarros@gmail.com>
2025-05-21 01:01:14 +0700

    final

--- shayna cosmetic buildwithangga html template/src/js/booking.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('input[type="tel"]').forEach((input) => {
-    input.addEventListener("input", function (e) {
-        this.value = this.value.replace(/[^0-9+]/g, "");
-    });
-});
